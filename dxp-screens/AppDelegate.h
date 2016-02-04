57953765fa26e79af5346c13ba929c22777ea198
dimplekoticha <dimple.koticha@liferay.com>
2016-02-04 11:37:24 -0600

    Initial Commit

---
+++ dxp-screens/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dxp-screens
+//
+//  Created by Dimple Koticha on 2016-02-04.
+//  Copyright © 2016 Liferay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dxp-screens/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  dxp-screens
+//
+//  Created by Dimple Koticha on 2016-02-04.
+//  Copyright © 2016 Liferay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
